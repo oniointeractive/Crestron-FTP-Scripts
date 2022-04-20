22485077be4aee3106e3bf6ba9d0d98137c401ec
Filip Horvat <40072838+raut3r@users.noreply.github.com>
2022-04-20 14:18:09 +0200

    Added scripts

---
+++ Crestron Console Secured.ps1
@@ -0,0 +1,13 @@
+﻿# variables
+$ip = # IP address of your processor 
+# e.g.   $ip = "192.168.1.110"
+$file = # Path to the local cpz file 
+# e.g.   $file = "C:\Users\Admin\Desktop\test.txt"
+$fileLocationOnProcessor = # Path to the remote file. Here you need to define how will the file be named.
+# e.g.   $fileLocationOnProcessor = "\program01\VisualStudioDebugger.cpz"
+$username = # It is only required if you have an authentication on your processore. If you do not have, you can remove this parts of the code, as well as -Secure
+# e.g. $username = "admin"
+$password = # The same comment as at username
+
+#command
+Send-FTPFile -Device $ip -LocalFile $file -RemoteFile $fileLocationOnProcessor -Secure -Username $username -Password $password
---
+++ Crestron Console Unsecured.ps1
@@ -0,0 +1,10 @@
+﻿# variables
+$ip = # IP address of your processor 
+# e.g.   $ip = "192.168.1.110"
+$file = # Path to the local cpz file 
+# e.g.   $file = "C:\Users\Admin\Desktop\test.txt"
+$fileLocationOnProcessor = # Path to the remote file. Here you need to define how will the file be named.
+# e.g.   $fileLocationOnProcessor = "\program01\VisualStudioDebugger.cpz"
+
+#command
+Send-FTPFile -Device $ip -LocalFile $file -RemoteFile $fileLocationOnProcessor -Secure
